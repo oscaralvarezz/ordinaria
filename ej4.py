3a31e05a485c6dd9045ed31f6600006e119d9539
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2023-01-17 11:17:23 +0000

    Update ej4.py

--- ej4.py
+++ ej4.py
@@ -1,18 +1,24 @@
-def usar_la_fuerza(mochila, num_objetos=0):
-  # En el caso de que la mochila este vacia nos da falso
-  if not mochila:
-    return False, num_objetos
+def main():
+#Creamos una función que nos de falso si la mochila esta vacía.
+  def usar_la_fuerza(mochila, numero_objetos=0):
+    if not mochila:
+      return False, numero_objetos
 
-  # Obtenemos el primer objeto
-  objeto = mochila[0]
-  mochila = mochila[1:]
+    #Sacamos de la mochila el primer objeto que haya
+    objeto = mochila[0]
+    mochila = mochila[1:]
 
-  # Si el sable encontramos el objeto si no utilizamos recursividad para ver si esta
-  if objeto == "sable de luz":
-    return True, num_objetos
-  return usar_la_fuerza(mochila, num_objetos + 1)
+    #Usamos una función recursiva para ver si encuentra el sable de luz o no, si no lo encuentra
+    #seguirá sacando objetos hasta encontrarlo.
+    if objeto == "sable de luz":
+      return True, numero_objetos
+    return usar_la_fuerza(mochila, numero_objetos + 1)
 
-# Comprobacion
-if __name__=="__main__":
-  lista = ["champiñon","europa","sable de luz", "la nueva liga"]
+  #Ahora comprobamos si está el sable de luz
+  #también se muestra el número de elementos extraídos hasta encontrarlo.
+  lista = ["espada láser","botella de agua","sable de luz", "bocadillo"]
   print(usar_la_fuerza(lista))
+
+if __name__=='__main__':
+    main()
+         
