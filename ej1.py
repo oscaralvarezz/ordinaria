cdfbf5be3c945bee882b83afa53da50a09aad97c
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2023-01-17 10:58:58 +0000

    Update ej1.py

--- ej1.py
+++ ej1.py
@@ -5,25 +5,25 @@ def main():
             self.name = name
             self.rango = rango
             self.calificacion()
-            print('stromtrooper creado con exito!')
+            print('¡Se ha creado un stromtrooper con exito!')
 
         def calificacion(self):
             for i in self.name:
                 if self.name == 'TK':
-                    print('{}codigo de legion {}'.format(self.name, self.rango))
+                    print('{}codigo de legión {}'.format(self.name, self.rango))
                 if self.name == '8':
                     print('{}identificador coherente{}'.format(self.name, self.rango))
                 if self.name == '6':
                     print('{}identificador de siglo {}'.format(self.name, self.rango))
                 if self.name == '5':
-                    print('{} tiene un numero de trooper {}'.format(self.name, self.rango))
+                    print('{} tiene un número de trooper {}'.format(self.name, self.rango))
                 if self.name == '4':
                     print('{}identificador de escuadra{}'.format(self.name, self.rango))
 
-    trooper1 = Stormtrooper('TK-421', 10)
-    trooper2 = Stormtrooper('8-8-8', 10)
-    trooper3 = Stormtrooper('6-6-6', 10)
-    lista = [trooper1, trooper2, trooper3]
+    Trooper1 = Stormtrooper('TK-421', 10)
+    Trooper2 = Stormtrooper('8-8-8', 10)
+    Trooper3 = Stormtrooper('6-6-6', 10)
+    lista = [Trooper1, Trooper2, Trooper3]
 
     for i in lista:
         i.calificacion()
