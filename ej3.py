b521fb6710ef43af9ac8fa75d49c8295ec6e71d6
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2023-01-17 11:08:38 +0000

    Update ej3.py

--- ej3.py
+++ ej3.py
@@ -4,16 +4,16 @@ class ItemsValiosos():
             self.nombre = nombre
             self.precio = precio
             self.caducidad = caducidad
-            print('Artefacto Valioso creado con exito!')
+            print('Un Artefacto Valioso se ha creado con exito!')
         
         def __str__(self):
-            return 'Artefacto Valioso: {} con peso {} Kg, precio: {} euros, y caducidad: {}'.format(self.nombre, self.peso, self.precio, self.caducidad)
+            return 'El Artefacto Valioso: {} con un peso de {} Kg, un precio de: {} euros, y una fecha de caducidad: {}'.format(self.nombre, self.peso, self.precio, self.caducidad)
         
-item1= ItemsValiosos(10, 'Artefacto 1', 100, '01/01/2020')
-item2= ItemsValiosos(20, 'Artefacto 2', 200, '01/01/2020')
-item3= ItemsValiosos(30, 'Artefacto 3', 300, '01/01/2020')
+elemento1= ItemsValiosos(10, 'Artefacto 1', 100, '01/01/2020')
+elemento2= ItemsValiosos(20, 'Artefacto 2', 200, '01/01/2020')
+elemento3= ItemsValiosos(30, 'Artefacto 3', 300, '01/01/2020')
 
-lista = [item1, item2, item3]
+lista = [elemento1, elemento2, elemento3]
 for i in lista: 
     print(i)
 
@@ -27,4 +27,4 @@ for i in lista:
         item.precio = nuevo_precio
         print('{} con nuevo precio de: {} euros'.format(item.nombre, item.precio))
     
-    cambiar_precio(item1, 500)
+    cambiar_precio(elemento1, 500)
