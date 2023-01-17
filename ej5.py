84269f42c85ca7d3e00bdbb4847a7eb2d23e7331
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2023-01-17 11:27:46 +0000

    Update ej5.py

--- ej5.py
+++ ej5.py
@@ -1,19 +1,24 @@
-def mochila(pesos, valores, capacidad):
-    n = len(pesos)
-    tabla = [[0] * (capacidad + 1) for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        for j in range(1, capacidad + 1):
-            if pesos[i - 1] > j:
-                tabla[i][j] = tabla[i - 1][j]
-            else:
-                tabla[i][j] = max(tabla[i - 1][j], tabla[i - 1][j - pesos[i - 1]] + valores[i - 1])
-    return tabla[n][capacidad]
+def main():
+    #creamos una función para determinar el valor máximo de la mochila según valores y pesos de los objetos.
+    def mochila(pesos, valores, capacidad):
+        z = len(pesos)
+        tabla = [[0] * (capacidad + 1) for _ in range(z + 1)]
+        for i in range(1, z + 1):
+            for j in range(1, capacidad + 1):
+                if pesos[i - 1] > j:
+                    tabla[i][j] = tabla[i - 1][j]
+                else:
+                    tabla[i][j] = max(tabla[i - 1][j], tabla[i - 1][j - pesos[i - 1]] + valores[i - 1])
+        return tabla[z][capacidad]
 
-
-
-if __name__=="__main__":
+    #Asignamos los pesos y valor al correspondiente objeto, y también la capacidad máxima de la mochila
     pesos = [12, 23, 11, 15, 7]
     valores =  [103, 60, 70, 5, 15] 
-    capacidad = 100
-    resultado = mochila(pesos, valores, capacidad)
-    print("El valor máximo que se puede llevar es: " , resultado)
+    capacidad_max = 60
+    resultado = mochila(pesos, valores, capacidad_max)
+
+    #mostramos el valor final de la mochila.
+    print("El valor máximo de los objetos que se pueden llevar en la mochila es de:",resultado)
+
+if __name__=="__main__":
+    main()
