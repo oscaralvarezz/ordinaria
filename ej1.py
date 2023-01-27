24bcc41400ecde58505351788f7ba7440aff9bc7
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2023-01-27 18:03:07 +0100

    Update ej1.py

--- ej1.py
+++ ej1.py
@@ -1,5 +1,6 @@
 def main():
     
+    #creamos la cladse Stormtrooper
     class Stormtrooper():
         def __init__(self, name, rango):
             self.name = name
@@ -7,10 +8,11 @@ def main():
             self.calificacion()
             print('¡Se ha creado un stromtrooper con exito!')
 
+        #creamos una función para calificar los trooper
         def calificacion(self):
             for i in self.name:
                 if self.name == 'TK':
-                    print('{}codigo de legión {}'.format(self.name, self.rango))
+                    print('{}código de legión {}'.format(self.name, self.rango))
                 if self.name == '8':
                     print('{}identificador coherente{}'.format(self.name, self.rango))
                 if self.name == '6':
@@ -20,12 +22,14 @@ def main():
                 if self.name == '4':
                     print('{}identificador de escuadra{}'.format(self.name, self.rango))
 
+    #a continuación asignamos valores a distintos trooper
     Trooper1 = Stormtrooper('TK-421', 10)
     Trooper2 = Stormtrooper('8-8-8', 10)
     Trooper3 = Stormtrooper('6-6-6', 10)
-    lista = [Trooper1, Trooper2, Trooper3]
+    Lista = [Trooper1, Trooper2, Trooper3]
 
-    for i in lista:
+    #calificamos los trooper
+    for i in Lista:
         i.calificacion()
 
 if __name__=='__main__':
